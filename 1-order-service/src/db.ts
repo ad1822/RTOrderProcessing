37a7d63fc866146909eaf3634346917e2140c643
Ayush Dumasia <ayushdumasia00@gmail.com>
2025-05-04 18:13:58 +0530

    refactor 🔨: Postgres

---
+++ 1-order-service/src/db.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+
+dotenv.config();
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+});
+
+export default pool;
