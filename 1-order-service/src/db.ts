a724eda556b2169095dd40c11829953642f9d122
Ayush Dumasia <ayushdumasia00@gmail.com>
2025-05-04 18:36:09 +0530

    refactor 🔨: Order Table

--- 1-order-service/src/db.ts
+++ 1-order-service/src/db.ts
@@ -11,4 +11,27 @@ const pool = new Pool({
   port: parseInt(process.env.DB_PORT || '5432', 10),
 });
 
+const createTableQuery = `
+  CREATE TABLE IF NOT EXISTS ORDER_DB (
+      id SERIAL PRIMARY KEY,
+      userId text(5),
+      orderId integer(3),
+      itemId integer(3),
+      orderAmount integer(5),
+      status text(100),
+  );
+`;
+
+pool
+  .query(createTableQuery)
+  .then((result) => {
+    console.log('Table created successfully:', result);
+  })
+  .catch((err) => {
+    console.error('Error creating table:', err);
+  })
+  .finally(() => {
+    pool.end();
+  });
+
 export default pool;
