e8cc2a231a828508db8f16ce0ae00a4ece13d1cf
Ayush Dumasia <ayushdumasia00@gmail.com>
2025-05-08 23:24:01 +0530

    refactor 🔨: DB

--- 2-inventory-service/init-inventory.sql
+++ 2-inventory-service/init-inventory.sql
@@ -1,17 +1,10 @@
--- init-inventory.sql
-CREATE TABLE IF NOT EXISTS inventory (
-  id SERIAL PRIMARY KEY,
-  itemId INTEGER,
-  quantity INT NOT NULL
-);
-
 INSERT INTO inventory (itemId, quantity) VALUES
 (1, 10),
-(2, 25);
--- (3, 50),
--- (4, 10),
--- (5, 25),
--- (6, 50),
--- (7, 10),
--- (8, 25),
--- (9, 50);
+(2, 25),
+(3, 50),
+(4, 10),
+(5, 25),
+(6, 50),
+(7, 10),
+(8, 25),
+(9, 50);
---
+++ 3-payment-service/src/db.ts
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+
+dotenv.config();
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+});
+
+const createTableQuery = `
+  CREATE TABLE IF NOT EXISTS payment (
+    id SERIAL PRIMARY KEY,
+    userId TEXT,
+    itemId INTEGER,
+    orderId INTEGER,
+    quantity INT NOT NULL,
+    status TEXT
+  );
+`;
+
+pool
+  .query(createTableQuery)
+  .then(() => {
+    console.log('✅ Table "Payment" created successfully.');
+  })
+  .catch((err) => {
+    console.error('❌ Error creating table:', err.message);
+  });
+
+export default pool;
